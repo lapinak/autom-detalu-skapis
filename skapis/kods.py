a74d9a680db5cdd80ddb620c91bae2d46e45ec22
KarinaAnna <48749638+lapinak@users.noreply.github.com>
2021-11-17 14:16:05 +0200

    Logger change

--- skapis/kods.py
+++ skapis/kods.py
@@ -8,10 +8,19 @@ import yaml
 from datetime import datetime
 from configparser import ConfigParser
 
+# Loading logging configuration
+with open('./log_worker.yaml', 'r') as stream:
+    config = yaml.safe_load(stream)
+
+logging.config.dictConfig(config)
+
+# Creating logger
+logger = logging.getLogger('root')
+
+"""
 a = random.uniform(0, 500)
 b = random.uniform(0, 10)
 
-"""
 print(a)
 print(b)
 
@@ -33,7 +42,7 @@ def access() :
         passwd = config.get('account', 'passw')
     except :
         #In case if soemthing has gone wrong with accessing the data in beginning stage there is a print with an error
-        print("We can't access the configuration file with your account details.")
+        logger.error("We can't access the configuration file with your account details.")
 
     #Prompts th user to enter their data
     try :
@@ -45,7 +54,7 @@ def access() :
         else :
             return False
     except :
-        print("Your provided date is not registered, contact the administrator.")
+        logger.debug("Your provided date is not registered, contact the administrator.")
 
 #The actual access method
 def authorization() :
@@ -58,7 +67,7 @@ def authorization() :
         elif access_type == '2' :
             return
     except:
-        print("Something went wrong.")
+        logger.error("Something went wrong.")
 
 
 #Calls out the method
@@ -67,4 +76,6 @@ authorization()
 try:
     weight = float(input("Enter the full weight: "))
     component = input("What component did you weight?")
-except: print("Make sure you have entered numbers and all the kommas are the dot (.) symobol")
+    count = int(weight/3)
+    logger.info("You've added ", count, component, " to the database")
+except: logger.debug("Make sure you have entered numbers and all the kommas are the dot (.) symobol")
