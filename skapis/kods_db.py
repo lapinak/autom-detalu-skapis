c994c5ece292e8314c35623a229706fd2e9e4a72
KarinaAnna <48749638+lapinak@users.noreply.github.com>
2021-11-21 22:04:44 +0200

    db

--- skapis/kods_db.py
+++ skapis/kods_db.py
@@ -115,7 +115,8 @@ except mysql.connector.Error as e:
     print("Error reading data from MySQL table", e)
 
 try:
-    query_two = "select weight from components where name = diode"
+    one = "select weight from components where name = '"+inputvalue+"'"
+    query_two = one
     
     cursor = get_cursor()
     cursor.execute(query_two)
