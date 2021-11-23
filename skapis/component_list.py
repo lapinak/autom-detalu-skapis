2363fb0fd4d6f9d6bbb48bde3c2cd3e2a867737a
kal <karii.a.lap123@gmail.com>
2021-11-23 19:06:25 +0000

    File to create the table with single component weights

--- skapis/component_list.py
+++ skapis/component_list.py
@@ -1,10 +1,12 @@
 import mysql.connector
 
+
+#Requires to fill in your DB connection data
 mydb = mysql.connector.connect(
-  host="127.0.0.1",
-  user="asteroids_user_s18",
-  password="Yoursuperpassword_s18",
-  database="asteroids_s18"
+  host=
+  user=
+  password=
+  database=
 )
 
 mycursor = mydb.cursor()
