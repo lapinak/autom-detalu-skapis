23ea7df3273bb79ad3bcfab7e0b661e7d5a4504f
kal <karii.a.lap123@gmail.com>
2021-11-23 19:04:00 +0000

    File for creating the main database for component count

--- skapis/skapis_db.py
+++ skapis/skapis_db.py
@@ -2,9 +2,9 @@ import mysql.connector
 
 mydb = mysql.connector.connect(
   host="127.0.0.1",
-  user="asteroids_user_s18",
-  password="Yoursuperpassword_s18",
-  database="asteroids_s18"
+  user=
+  password=
+  database=
 )
 
 mycursor = mydb.cursor()
